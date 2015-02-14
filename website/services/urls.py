b3c1bcd74273ce0ee710bc7228279c9239fcc211
kadikraman <hellokadi@gmail.com>
2015-02-14 11:34:22 +0000

    Fixing links and highlights

--- website/services/urls.py
+++ website/services/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^events/', views.EventsView.as_view(), name='events'),
     url(r'^photoshoots/', views.PhotoshootsView.as_view(), name='photoshoots'),
     url(r'^stylistics/', views.StylisticsView.as_view(), name='stylistics'),
-    url(r'^public_relations', views.PublicRelationsView.as_view(), name='public_relations'),
+    url(r'^public_relations/', views.PublicRelationsView.as_view(), name='public_relations'),
 )
